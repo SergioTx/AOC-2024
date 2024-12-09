9e6a7e91931a7941f83a2bb0fceb023b743f1fc1
SergioTx <txitxo37@gmail.com>
2024-12-09 08:30:12 +0100

    refactor: unify day 7 branches

--- src/day7/7.ts
+++ src/day7/7.ts
@@ -1,90 +1,53 @@
 const file = Deno.readTextFileSync('src/day7/input7.txt');
 const input = file.split('\n').map((line) => line.split(': '));
 
-function getSumMultiplyAllOperatorCombinations(numOperators: number): string[][] {
-  const totalOperators = 2 ** numOperators;
-  let operators = [['*'], ['+']];
+const SUM = '+';
+const MULTIPLY = '*';
+const CONCAT = '||';
+const OPERATORS_A = [[MULTIPLY], [SUM]];
+const OPERATORS_B = [[MULTIPLY], [SUM], [CONCAT]];
+
+function getAllOperatorCombinations(numOperators: number, operatorOptions: string[][]): string[][] {
+  const totalOperators = operatorOptions.length ** numOperators;
+  let operators = structuredClone(operatorOptions);
 
   while (operators.length < totalOperators) {
-    operators = operators.flatMap((el) => [
-      [...el, '+'],
-      [...el, '*'],
-    ]);
+    operators = operators.flatMap((el) => operatorOptions.map((op) => [...el, ...op]));
   }
 
   return operators;
 }
 
-function isValidSumMultiplyLine(total: number, values: number[]): boolean {
+function isValidLine(total: number, values: number[], operatorOptions: string[][]): boolean {
   const numOperators = values.length - 1;
-  const operatorCombinations = getSumMultiplyAllOperatorCombinations(numOperators);
+  const operatorCombinations = getAllOperatorCombinations(numOperators, operatorOptions);
 
   return operatorCombinations.some((operators) => {
     const combinationTotal = values.reduce((acc, curr, index) => {
       const operator = operators[index - 1];
-      if (operator === '*') return acc * curr;
-      return acc + curr;
+      if (operator === MULTIPLY) return acc * curr;
+      if (operator === SUM) return acc + curr;
+      if (operator === CONCAT) return +(String(acc) + String(curr));
+      return acc;
     });
     return combinationTotal === total;
   });
 }
 
-function getValidSumMultiplySum(input: string[][]) {
+function getValidSum(input: string[][], operatorOptions: string[][]) {
   const filtered = input.filter((line) => {
     const total = +line.at(0)!;
     const values = line.at(1)!.trim().split(' ').map(Number);
 
-    return isValidSumMultiplyLine(total, values);
+    return isValidLine(total, values, operatorOptions);
   });
 
   const sum = filtered.reduce((acc, line) => acc + +line.at(0)!, 0);
   return sum;
 }
 
-function getSumMultiplyConcatAllOperatorCombinations(numOperators: number): string[][] {
-  const totalOperators = 3 ** numOperators;
-  let operators = [['*'], ['+'], ['||']];
-
-  while (operators.length < totalOperators) {
-    operators = operators.flatMap((el) => [
-      [...el, '+'],
-      [...el, '*'],
-      [...el, '||'],
-    ]);
-  }
-
-  return operators;
-}
-
-function isValidSumMultiplyConcatLine(total: number, values: number[]): boolean {
-  const numOperators = values.length - 1;
-  const operatorCombinations = getSumMultiplyConcatAllOperatorCombinations(numOperators);
-
-  return operatorCombinations.some((operators) => {
-    const combinationTotal = values.reduce((acc, curr, index) => {
-      const operator = operators[index - 1];
-      if (operator === '*') return acc * curr;
-      if (operator === '+') return acc + curr;
-      return +(String(acc) + String(curr));
-    });
-    return combinationTotal === total;
-  });
-}
-
-function getValidSumMultiplyConcatSum(input: string[][]) {
-  const filtered = input.filter((line) => {
-    const total = +line.at(0)!;
-    const values = line.at(1)!.trim().split(' ').map(Number);
-
-    return isValidSumMultiplyConcatLine(total, values);
-  });
-
-  const sum = filtered.reduce((acc, line) => acc + +line.at(0)!, 0);
-  return sum;
-}
-
-const resultA = 'DONE'; // getValidSumMultiplySum(input);
-const resultB = 'DONE'; // getValidSumMultiplyConcatSum(input);
+const resultA = getValidSum(input, OPERATORS_A);
+const resultB = getValidSum(input, OPERATORS_B);
 
 console.log('7A: ', resultA);
 console.log('7B: ', resultB);
