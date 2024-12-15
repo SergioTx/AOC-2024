8d9efff788fc1dd8e261749534b2b104c55d39e3
SergioTx <txitxo37@gmail.com>
2024-12-16 00:00:21 +0100

    chore: add instructions for days 13 and 14

---
+++ src/day13/13.ts
@@ -0,0 +1,8 @@
+const file = Deno.readTextFileSync('src/day13/input13.txt');
+const input = file.split('\n');
+
+const resultA = '';
+const resultB = '';
+
+console.log('13A: ', resultA);
+console.log('13B: ', resultB);
---
+++ src/day14/14.ts
@@ -0,0 +1,8 @@
+const file = Deno.readTextFileSync('src/day14/input14.txt');
+const input = file.split('\n');
+
+const resultA = '';
+const resultB = '';
+
+console.log('14A: ', resultA);
+console.log('14B: ', resultB);
